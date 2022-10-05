bda0809468f0b959dfe3fdede2201fb1ab4a81b3
Go-MinSung <bestfriend1562@gmail.com>
2022-10-05 22:03:06 +0900

    thymeleaf 유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -7,7 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -55,9 +57,15 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
     @Component("helloBean")
-    static class HelloBean{
-        public String hello(String data){
+    static class HelloBean {
+        public String hello(String data) {
             return "hello " + data;
         }
     }
